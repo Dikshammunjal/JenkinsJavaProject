52dfc65709ce17f481a828147d4f3d3b7d550097
Dikshammunjal <57708209+Dikshammunjal@users.noreply.github.com>
2021-10-12 10:50:56 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	public static void main(String[] s) {
-		System.out.println("Hello World");
+		System.out.println("Hello World Diksha");
 
 		for(int i=0; i<=10; i++) {
 			System.out.println(i);
